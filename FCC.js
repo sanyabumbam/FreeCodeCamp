d55bc0e3b208ebb1c28001a54c403d31ebe9c1b1
sanyabumbam <102165817+sanyabumbam@users.noreply.github.com>
2022-03-28 16:25:52 +0300

    Add files via upload

---
+++ FCC.js
@@ -0,0 +1,188 @@
+var a = 5;
+var b = 10;
+var c = 'I am a';
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+
+let catName = "Oliver";
+let catSound = "Meow!";
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+
+const sum = 10 + 10;
+
+
+const difference = 45 - 33;
+
+
+const product = 8 * 10;
+
+
+const quotient = 66 / 33;
+
+
+let myVar = 87;
+
+// Only change code below this line
+myVar++;
+
+
+let myVar = 11;
+
+// Only change code below this line
+myVar--
+
+
+
+const ourDecimal = 5.7;
+
+// Only change code below this line
+let myDecimal = 5.7;
+
+
+const remainder = 11 % 3;
+
+
+const product = 2.0 * 2.5;
+
+
+
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+
+
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+
+
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+
+
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+
+
+const myStr = "This is the start. " + "This is the end.";
+
+
+let myStr = "This is the first sentence. "
+myStr += "This is the second sentence.";
+
+
+
+const myName = "Aleks";
+const myStr = "My name is " + myName + ", and I am well!";
+
+
+const someAdjective = "Nothing ";
+let myStr = "Learning to code is";
+myStr += someAdjective;
+
+
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+
+lastNameLength = lastName.length;
+console.log(lastName.length);
+
+
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0];
+
+
+
+const lastName = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2];
+
+
+
+
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+
+
+
+const lastName = "Lovelace";
+
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName - 2]; // Change this line
+
+
+
+const myArray = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [[10, 11, 12], 13, 14],
+];
+
+const myData = myArray[2][1];
+
+
+
+const myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+myArray.push(["dog", 3]);
